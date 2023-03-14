6b363c6a84726c6ac37c0974faee07c928d7ee3e
Kamran <127879566+kamranahmadd@users.noreply.github.com>
2023-03-14 18:56:16 +0100

    Create practice.py

---
+++ practice.py
@@ -0,0 +1,8 @@
+import tensorflow as tf
+from tensorflow.keras.layers import Input, Dense, SimpleRNN, LSTM, GRU, Flatten, GlobalMaxPool1D
+from tensorflow.keras.models import Model
+from tensorflow.keras.optimizers import Adam, SGD
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
